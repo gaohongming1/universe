229a2b003ff456d66decb81655761467a6475d5f
gaohongming1 <gaohongming3@jd.com>
2020-12-07 20:17:42 +0800

    分支限界法

--- origin-star/src/main/java/com/universe/origin/star/special/branch/BackPack02.java
+++ origin-star/src/main/java/com/universe/origin/star/special/branch/BackPack02.java
@@ -15,12 +15,11 @@ public class BackPack02 {
     public static void main(String[] args) {
         int[] weight = new int[]{2, 5, 4, 2};
         int[] value = new int[]{6, 3, 5, 4};
-        int[] s = Arrays.copyOf(weight, weight.length);
-        s[2] = 100;
-        System.out.println(Arrays.toString(weight));
-        System.out.println(Arrays.toString(s));
+        int[] bestValueStatus = new int[weight.length];
+        BackPack02 backPack02 = new BackPack02();
 
-
+        System.out.println(backPack02.calc(weight,value,10,bestValueStatus));
+        System.out.println(Arrays.toString(bestValueStatus));
     }
 
     /**
@@ -29,58 +28,82 @@ public class BackPack02 {
      * @param weight
      * @param value
      */
-    public void calc(int[] weight, int[] value, int maxWeight) {
+    public Integer calc(int[] weight, int[] value, int maxWeight, int[] bestValueStatus) {
 
         //定义当前最优解
         int bestValue = 0;
 
         // 第一个节点剩余物品价值
         int surplusValue = 0;
-        int surplusWeight = 0;
+        int surplusWeight = maxWeight;
         int length = weight.length;
         // 节点的状态 初始节点都是未加入状态
         int[] currentValueStatus = new int[length];
         // 计算剩余物品的价值
         for (int i = 0; i < weight.length; i++) {
             surplusValue += value[i];
-            surplusWeight += weight[i];
         }
         Node firstNode = new Node(0, surplusValue, surplusWeight, currentValueStatus, 0);
-        //第一个几点入队
         Queue<Node> queue = new LinkedList<Node>();
         queue.add(firstNode);
+
         // 深度遍历出队节点将子节点入队
         while (!queue.isEmpty()) {
             Node currentNode = queue.poll();
             int index = currentNode.index;
 
-            // 判断当前节点能否加入 查看剩余容量是否大于当前节点
-            if (currentNode.surplusValue <= weight[index]) {
-                // 加入则计算需要构造的新的Node
-                int nowCurrentValue = currentNode.currentValue + weight[index];
+            /**
+             * 判断这条路是不是走到终点了，要么是没有容量了要么是走到叶子节点了
+             * 然后替换最优解。拿到结果之后代表当前路径已经走完，不再生成左右节点
+             */
+            if (index > (weight.length - 1) || currentNode.surplusWight == 0) {
+                if (currentNode.currentValue >= bestValue) {
+                    bestValue = currentNode.currentValue;
+                    //注意不要用这个，这个是新开辟了地址
+                    //bestValueStatus = Arrays.copyOf(currentNode.currentValueStatus, length);
+
+                    for (int i = 0; i < length; i++) {
+                        bestValueStatus[i] = currentNode.currentValueStatus[i];
+                    }
+                }
+                continue;
+            }
+
+            /**
+             * 相当于剪枝函数，避免生成过多的节点
+             */
+            if ((currentNode.currentValue + currentNode.surplusValue) <bestValue ){
+                continue;
+            }
+
+            /**
+             *判断当前节点能否加入 查看剩余容量是否大于当前节点,大于则代表当前节点可以加入
+             */
+            if (currentNode.surplusWight >= weight[index]) {
+                int nowCurrentValue = currentNode.currentValue + value[index];
                 int nowSurplusValue = currentNode.surplusValue - value[index];
                 int nowSurplusWeight = currentNode.surplusWight - weight[index];
                 int[] nowCurrentValueStatus = Arrays.copyOf(currentNode.currentValueStatus, length);
+                nowCurrentValueStatus[index] = 1;
                 Node addNode = new Node(nowCurrentValue, nowSurplusValue, nowSurplusWeight, nowCurrentValueStatus, index + 1);
-                //写个节点入队
                 queue.add(addNode);
             }
-            // 右孩子入队 判断剩余物品价值是否大于当前最好的
-            // 不管当前的节点入不入队，前置的节点加上剩余物品的总价值大于等于当前最优值才能继续走下去
+
             /**
              * 如果当前节点不加入则 已经加入的和剩余物品的总价值应该大于当前的最优值才能走下去
              */
             int valueWithoutCurrentNode = currentNode.currentValue + currentNode.surplusValue - value[index];
-            if (valueWithoutCurrentNode >= bestValue){
-
+            if (valueWithoutCurrentNode >= bestValue) {
+                int nowCurrentValue = currentNode.currentValue;
+                int nowSurplusValue = currentNode.surplusValue - value[index];
+                int nowSurplusWeight = currentNode.surplusWight;
+                int[] nowCurrentValueStatus = Arrays.copyOf(currentNode.currentValueStatus, length);
+                nowCurrentValueStatus[index] = 0;
+                Node addNode = new Node(nowCurrentValue, nowSurplusValue, nowSurplusWeight, nowCurrentValueStatus, index + 1);
+                queue.add(addNode);
             }
-
-
-
-
-
         }
-
+        return bestValue;
 
     }
 
@@ -114,7 +137,6 @@ public class BackPack02 {
             this.currentValueStatus = currentValueStatus;
             this.index = index;
         }
-
 
 
         public Integer getCurrentValue() {
