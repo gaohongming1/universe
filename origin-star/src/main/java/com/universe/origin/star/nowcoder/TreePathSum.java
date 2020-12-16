014ba8ac0333299e7bd214fc922f102973878319
guoshoujing <1075185785@qq.com>
2020-12-16 11:55:10 +0800

    add code TreePathSum

--- origin-star/src/main/java/com/universe/origin/star/nowcoder/TreePathSum.java
+++ origin-star/src/main/java/com/universe/origin/star/nowcoder/TreePathSum.java
@@ -1,7 +1,7 @@
 package com.universe.origin.star.nowcoder;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 
 /**
  * 给定一个二叉树和一个值sum，请找出所有的根节点到叶子节点的节点值之和等于sum的路径，
@@ -20,26 +20,28 @@ public class TreePathSum {
      * @return int整型ArrayList<ArrayList<>>
      */
     public ArrayList<ArrayList<Integer>> pathSum (TreeNode root, int sum) {
+        if(root == null){
+            return new ArrayList<>();
+        }
         ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-        List<Integer> path = new ArrayList<>();
+        LinkedList<Integer> path = new LinkedList<>();
         dfs(root, 0, sum, result, path);
         return result;
     }
 
-    private void dfs(TreeNode node, int pathSum, int sum, ArrayList<ArrayList<Integer>> result, List<Integer> path){
-        if(node == null || pathSum > sum){
+    private void dfs(TreeNode node, int pathSum, int sum, ArrayList<ArrayList<Integer>> result, LinkedList<Integer> path){
+        if(node == null){
             return;
         }
-        path.add(node.val);
-        if((pathSum += node.val) == sum && node.left == null && node.right == null){
+        if(path.add(node.val) && (pathSum += node.val) == sum
+                && node.left == null && node.right == null){
             result.add(new ArrayList<>(path));
             return;
         }
-        if(node.left != null){
+        if(pathSum < sum){
             dfs(node.left, pathSum, sum, result, path);
-        }
-        if(node.right != null){
             dfs(node.right, pathSum, sum, result, path);
         }
+        path.removeLast();
     }
 }
