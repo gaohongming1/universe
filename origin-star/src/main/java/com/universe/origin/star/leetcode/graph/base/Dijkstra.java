6b0a70bb12ba5fca8419cb8b287ea32e4a9f2f2c
gaohongming1 <gaohongming3@jd.com>
2021-03-16 20:45:16 +0800

    122

---
+++ origin-star/src/main/java/com/universe/origin/star/leetcode/graph/base/Dijkstra.java
@@ -0,0 +1,11 @@
+package com.universe.origin.star.leetcode.graph.base;
+
+/**
+ * Dijkstra 算法，又叫迪科斯彻算法（Dijkstra），
+ * 算法解决的是有向图中单个源点到其他顶点的最短路径问题。
+ */
+public class Dijkstra {
+    public static void main(String[] args) {
+
+    }
+}
